ef973beadef190e4090d909c9311267ca22c23dc
Akhil-py <ramshankar.akhil@gmail.com>
2024-12-22 11:33:16 +0530

    Implemented display update function and scaling

--- window.cpp
+++ window.cpp
@@ -1,20 +1,30 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+/**
+ * A class to handle UI pixel generation using the SDL2 library
+ */
 class Window {
 public:
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	SDL_Texture* texture;
+	int WIDTH;
+	int HEIGHT;
+	int SCALE;
 
-	Window(const int width, const int height) {
+	Window(const int width, const int height, const int scale) {
+		WIDTH = width;
+		HEIGHT = height;
+		SCALE = scale;
+
 		SDL_Init(SDL_INIT_VIDEO);
 		
-		window = SDL_CreateWindow("chip8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
+		window = SDL_CreateWindow("Chip 8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH*SCALE, HEIGHT*SCALE, 0);
 
 		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-		texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+		texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
 	}
 
 	~Window() {
@@ -24,30 +34,37 @@ public:
 		SDL_Quit();
 	}
 
+	/**
+	 * Used to update the display with pixel data from buffer
+	 */
 	void update(void const* buffer) {
+		SDL_UpdateTexture(texture, NULL, buffer, WIDTH*sizeof(uint32_t)); 
 
+		SDL_Rect destRect = {0, 0, WIDTH*SCALE, HEIGHT*SCALE}; // Scaling
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture, NULL, &destRect);
+		SDL_RenderPresent(renderer);
 	}
-
 };
 
 
 /**
- * The main function is only used to test the SDL library
+ * The main function is only used to test the SDL library and the window class functions
  */
 int main(int argc, char *argv[])
 {
 	const int WIDTH = 64;
 	const int HEIGHT = 32;
-	const int SCALE = 20;
-	
-	// returns zero on success else non-zero
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		printf("error initializing SDL: %s\n", SDL_GetError());
-	}
-	SDL_Window* window = SDL_CreateWindow("chip8",
-									SDL_WINDOWPOS_CENTERED,
-									SDL_WINDOWPOS_CENTERED,
-									WIDTH*SCALE, HEIGHT*SCALE, 0);
+	const int SCALE = 25;
+	uint32_t display[64 * 32]{0xFF0000FF};
+
+	Window window = Window(WIDTH, HEIGHT, SCALE);
+
+	display[5 * WIDTH + 10] = 0xFFFFFFFF; // Pixel at (10, 5)
+    display[15 * WIDTH + 20] = 0xFFFFFFFF; // Pixel at (20, 15)
+
+	window.update(display);
 
 	SDL_Event windowEvent;
 
@@ -59,8 +76,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	window.~Window();
 
 	return 0;
 }
