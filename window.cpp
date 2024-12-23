46e1dcfc6dfedb8d0716f4cf4efa144dc0535345
Akhil-py <ramshankar.akhil@gmail.com>
2024-12-23 11:26:29 +0530

    Added SDL sound functionality

--- window.cpp
+++ window.cpp
@@ -9,35 +9,43 @@ public:
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 	SDL_Texture* texture;
+	SDL_AudioDeviceID audioDevice;
 	int WIDTH;
 	int HEIGHT;
 	int SCALE;
 
-	Window(const int width, const int height, const int scale) {
+	Window (const int width, const int height, const int scale) {
 		WIDTH = width;
 		HEIGHT = height;
 		SCALE = scale;
 
 		SDL_Init(SDL_INIT_VIDEO);
-		
 		window = SDL_CreateWindow("Chip 8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH*SCALE, HEIGHT*SCALE, 0);
-
 		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
 		texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+
+		SDL_Init(SDL_INIT_AUDIO);
+		SDL_AudioSpec desiredSpec{};
+		desiredSpec.freq = 44100;
+		desiredSpec.format = AUDIO_S16SYS;
+		desiredSpec.channels = 1;
+		desiredSpec.samples = 2048;
+		desiredSpec.callback = audioCallback; 
+		audioDevice = SDL_OpenAudioDevice(NULL, 0, &desiredSpec, NULL, 0);
 	}
 
-	~Window() {
+	~Window () {
 		SDL_DestroyTexture(texture);
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
+		SDL_CloseAudioDevice(audioDevice);
 		SDL_Quit();
 	}
 
 	/**
 	 * Used to update the display with pixel data from buffer
 	 */
-	void update(void const* buffer) {
+	void update (void const* buffer) {
 		SDL_UpdateTexture(texture, NULL, buffer, WIDTH*sizeof(uint32_t)); 
 
 		SDL_Rect destRect = {0, 0, WIDTH*SCALE, HEIGHT*SCALE}; // Scaling
@@ -48,9 +56,41 @@ public:
 	}
 
 	/**
+	 * Sound functionality - generate square wave
+	 */
+	static void audioCallback (void* userdata, Uint8* stream, int len) {
+		const int SAMPLE_RATE = 44100;
+		const int AMPLITUDE = 28000;
+		const int FREQUENCY = 440;
+		
+		static int phase = 0;
+		Sint16* buffer = (Sint16*)stream;
+		int samples = len / 2;
+
+		for (int i = 0; i < samples; ++i) {
+			buffer[i] = (phase < (SAMPLE_RATE / FREQUENCY) / 2) ? AMPLITUDE : -AMPLITUDE;
+			phase = (phase + 1) % (SAMPLE_RATE / FREQUENCY);
+		}
+	}
+
+	/**
+	 * Start playing beep
+	 */
+	void startBeep () {
+		SDL_PauseAudioDevice (audioDevice, 0);
+	}
+
+	/**
+	 * Stop playing beep
+	 */
+	void stopBeep () {
+		SDL_PauseAudioDevice (audioDevice, 1);
+	}
+
+	/**
 	 * Keypad functionality
 	 */
-	bool processInput(size_t* keys) {
+	bool processInput (size_t* keys) {
 		SDL_Event event;
 		bool quit = false;
 
@@ -174,7 +214,7 @@ public:
 /**
  * The main function is only used to test the SDL library and the window class functions
  */
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 	const int WIDTH = 64;
 	const int HEIGHT = 32;
@@ -182,6 +222,8 @@ int main(int argc, char *argv[])
 	uint32_t display[64 * 32]{0xFF0000FF};
 
 	Window window = Window(WIDTH, HEIGHT, SCALE);
+
+	window.startBeep();
 
 	display[5 * WIDTH + 10] = 0xFFFFFFFF; // Pixel at (10, 5)
     display[15 * WIDTH + 20] = 0xFFFFFFFF; // Pixel at (20, 15)
@@ -198,6 +240,8 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
+
+	window.stopBeep();
 
 	window.~Window();
 
